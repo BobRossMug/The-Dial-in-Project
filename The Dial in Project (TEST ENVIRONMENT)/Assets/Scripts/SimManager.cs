817c67a6cf23f20075ecaaaf22eeabf7213cbadc
BobRossMug <onurmuhurdar@gmail.com>
2025-01-19 23:02:54 +0100

    GameManager Basics

---
+++ The Dial in Project (TEST ENVIRONMENT)/Assets/Scripts/SimManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimManager : MonoBehaviour {
+
+    public static SimManager Instance;
+
+    [Header("Game States")]
+    public bool isGrinding;
+    public bool isBrewing;
+    public bool resultsScreen;
+    public bool settingsScreen;
+
+    [Header("Coffee Types")] 
+    
+    public float coffeeType;
+    public float roastConsistency;
+    
+    public float currentQuality = 0f;
+    public float maxQuality = 100f;
+    
+    
+    
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    } /* end Awake */ //
+    
+    void Start() {
+        
+        ResetGame();
+
+    }/* end Start */ ////
+
+    
+    
+    public void ResetGame() {
+        isGrinding = false;
+        isBrewing = false;
+        currentQuality = 0f;
+    } /* end ResetGame */ //
+    
+    public void ModifyCoffeeQuality(float amount) {
+        //currentQuality += amount;
+        //currentQuality = Mathf.Clamp(currentQuality, 0, maxQuality);
+        Debug.Log("Coffee quality is now: " + currentQuality);
+    }/* end ModifyCoffeQuality */ //
+    
+    
+    
+    
+    void Update() {
+        
+    }/* end Update */ ////
+                      
+                      
+} /* end Script */ /////

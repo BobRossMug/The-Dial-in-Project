ca768c095fe85aec8b63dbfac261f9ce9f4178d7
BobRossMug <onurmuhurdar@gmail.com>
2025-01-19 23:04:33 +0100

    Coffee Bean Stuff

---
+++ The Dial in Project (TEST ENVIRONMENT)/Assets/Scripts/CoffeeBean.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeBean : MonoBehaviour {
+    
+    public string beanName;
+    public string origin;
+    public float roastLevel;   // 0 = light roast, 100 = dark roast
+    public string flavorNotes;
+
+    public float GetBaseQuality() {
+        // Light roasts might have higher “complexity,” dark roasts might have more “body” etc.
+        float baseQuality = 50f + (roastLevel - 50f) * 0.5f;
+        return Mathf.Clamp(baseQuality, 0f, 100f);
+    } /* end GetBaseQualityt */ ///
+    
+}/* end Script */ ////

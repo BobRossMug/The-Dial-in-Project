1bf6e353a0bd049ecac7b4fc0dbdf37ddb6cfe5a
BobRossMug <onurmuhurdar@gmail.com>
2025-01-28 07:47:04 +0100

    buildSettings

---
+++ The Dial in Project (TEST ENVIRONMENT)/Assets/Scripts/BuildScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class BuildScript {
+    [MenuItem("Build/Build Project")]
+    public static void BuildGame() {
+        string path = Path.Combine(Application.dataPath, "../Builds/MyGame.exe");
+        
+        Debug.Log("Building game to: " + path); 
+
+        BuildPipeline.BuildPlayer(
+            new string[] { "Assets/Scenes/MainScene.unity" }, 
+            path, 
+            BuildTarget.StandaloneWindows, 
+            BuildOptions.None
+        );
+
+        EditorUtility.RevealInFinder(path);
+    }
+}
